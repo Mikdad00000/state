ba02dc194e3b2c026177de7ec4e0107e7098c702
Mikdad Rahman <mikdad00000@gmail.com>
2022-09-24 11:31:29 +0600

    add Links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Counter from './Counter';
+import { Routes, Route, Link } from "react-router-dom";
 import Form from './Form';
+import Links from './Links';
 function App() {
   return (
     <>
-      <Form />
+      <Routes>
+        <Route path="/form" element={<Form />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/" element={<Links />} />
+      </Routes>
     </>
   );
 }
