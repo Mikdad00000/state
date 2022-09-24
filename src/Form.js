44ef17401eb0ecea7533cd935e60c4682f223523
Mikdad Rahman <mikdad00000@gmail.com>
2022-09-24 12:16:26 +0600

    css

--- src/Form.js
+++ src/Form.js
@@ -6,48 +6,51 @@ function Form() {
     const [phone, setPhone] = useState("");
     const [password, setPassword] = useState("");
     return (
-        <div className="flex flex-col justify-center h-full">
-            <div className="flex h-full ">
-                <label className="text-xl">User Name</label>
-                <input
-                    type="text"
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                    className="h-[75%] border"
-                />
+        <div className="h-screen justify-center w-full">
+            <div className="flex flex-col w-[50%] justify-center h-full  border shadow-md p-5">
+                <div className="flex h-[20%] w-full justify-between m-5 ">
+                    <label className="text-2xl ">User Name</label>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        className="h-full border w-9/12 "
+                    />
+                </div>
+                <div className="flex h-[20%] w-full justify-between m-5 ">
+                    <label className="text-xl">Email</label>
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        className="h-full border w-9/12 "
+                    />
+                </div>
+                <div className="flex h-[20%] w-full justify-between m-5 ">
+                    <label className="text-xl">Phone Number</label>
+                    <input
+                        type="text"
+                        value={phone}
+                        onChange={e => setPhone(e.target.value)}
+                        className="h-full border w-9/12 "
+                    />
+                </div>
+                <div className="flex h-[20%] w-full justify-between m-5 ">
+                    <label className="text-xl">Password</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        className="h-full border w-9/12 "
+                    />
+                </div>
+                <p> {name} </p>
+                <p> {email} </p>
+                <p> {phone} </p>
+                <p> {password} </p>
             </div>
-            <div className="flex h-full ">
-                <label className="text-xl">Email</label>
-                <input
-                    type="text"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    className="h-[75%] border"
-                />
-            </div>
-            <div className="flex h-full ">
-                <label className="text-xl">Phone Number</label>
-                <input
-                    type="text"
-                    value={phone}
-                    onChange={e => setPhone(e.target.value)}
-                    className="h-[75%] border"
-                />
-            </div>
-            <div className="flex h-full ">
-                <label className="text-xl">Password</label>
-                <input
-                    type="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    className="h-[75%] border"
-                />
-            </div>
-            <p> {name} </p>
-            <p> {email} </p>
-            <p> {phone} </p>
-            <p> {password} </p>
         </div>
+
     )
 };
 export default Form;
